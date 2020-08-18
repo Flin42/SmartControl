e8b19a402ad64ef018a8e39d2a65137a1d64be24
flin42 <feiyanglin2010@gmail.com>
2020-08-18 14:30:16 -0600

    Input Semi Mapped

--- src/main/java/com/datasoldier/smartcontrol/test/GreetingController.java
+++ src/main/java/com/datasoldier/smartcontrol/test/GreetingController.java
@@ -2,18 +2,33 @@ package com.datasoldier.smartcontrol.test;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class GreetingController{
 
-    @RequestMapping("/greeting")
+    @GetMapping("/greeting")
     public String greeting() {
-
         return "greeting";
+    }
+
+    @PostMapping("/greeting")
+    public String result(@RequestParam("txt") String s){
+        if(s.equalsIgnoreCase("up")) {
+            System.out.println("up");
+        } else if(s.equalsIgnoreCase("right")) {
+            System.out.println("right");
+        } else if(s.equalsIgnoreCase("down")) {
+            System.out.println("down");
+        } else if(s.equalsIgnoreCase("left")) {
+            System.out.println("left");
+        } else {
+            System.out.println("not a direction");
+        }
+        System.out.println(s);
+        return "result";
+
     }
 
 
